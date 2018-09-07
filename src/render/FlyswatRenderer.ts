09bcd9eda46ba031da62d0bb5968bc449a62f2f0
kamsac <kamsac@users.noreply.github.com>
2018-09-07 19:18:26 +0200

    Draw flyswatter while not attacking.

--- src/render/FlyswatRenderer.ts
+++ src/render/FlyswatRenderer.ts
@@ -4,6 +4,7 @@ import Sprites from '../sprites/Sprites';
 import Point from '../../helpers/Point';
 import MainCharacter from '../main-character/MainCharacter';
 import {playerSpriteSize} from './MainCharacterRenderer';
+import Direction from '../../helpers/Direction';
 
 export default class FlyswatRenderer {
     private context: CanvasRenderingContext2D;
@@ -16,25 +17,26 @@ export default class FlyswatRenderer {
 
         this.renderCrosshair(aabb);
 
-        if (world.flyswat.isHitting) {
-            this.renderFlyswat(world, aabb);
-        }
+        this.renderFlyswat(world, aabb, world.flyswat.isHitting);
     }
 
     private renderCrosshair(aabb: AABB): void {
-        this.context.fillStyle = 'rgba(255,64,64,0.5)';
+        this.context.fillStyle = 'rgba(0,0,0,0.15)';
         this.context.fillRect(aabb.x, aabb.y, aabb.width, aabb.height);
     }
 
-    private renderFlyswat(world: World, aabb: AABB): void {
+    private renderFlyswat(world: World, aabb: AABB, isHitting: boolean): void {
         const playerHandPosition: Point = getPlayerRightHandPosition(world.player);
+        const inMidairOffset: number = isHitting ? 0 : -flyswatMidairHeight - world.player.positionZ;
+
         this.context.lineWidth = 2;
         this.context.strokeStyle = '#120f0c';
         this.context.beginPath();
         this.context.moveTo(playerHandPosition.x, playerHandPosition.y);
-        this.context.lineTo(world.flyswat.position.x, world.flyswat.position.y);
+        this.context.lineTo(world.flyswat.position.x, world.flyswat.position.y + inMidairOffset);
+
         this.context.stroke();
-        this.context.drawImage(Sprites.flyswat, aabb.x, aabb.y, aabb.width, aabb.height);
+        this.context.drawImage(Sprites.flyswat, aabb.x, aabb.y + inMidairOffset, aabb.width, aabb.height);
     }
 }
 
@@ -62,6 +64,8 @@ function getPlayerRightHandPosition(player: MainCharacter): Point {
 }
 
 export function isFlyswatBehindPlayer(player: MainCharacter): boolean {
-    const direction = player.forward.direction();
+    const direction: Direction = player.forward.direction();
     return (direction === 'up' || direction === 'left');
 }
+
+const flyswatMidairHeight: number = 20;
